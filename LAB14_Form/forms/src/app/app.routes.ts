6b2e600c39a0aa0a0beb209f6d5ad1f96427b46c
giobitty <81569090+giobitty@users.noreply.github.com>
2024-08-12 00:34:23 -0400

    labs

--- LAB14_Form/forms/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserComponent } from './user/user.component';  // Import your UserComponent
-
-export const routes: Routes = [
-    { path: '', redirectTo: '/user', pathMatch: 'full' },  // Redirect to /user by default
-    { path: 'user', component: UserComponent }  // Define the route for UserComponent
-  
-];
--- firstAngular/src/app/book/book.model.ts
+++
@@ -1,5 +0,0 @@
-export class Book{
-    constructor(public id?:number, public name?:string,public writer?:string,public price?:number){
-        
-    }
-}
